4009e54d124d4568c70b0bc4fd58d3d418ed3eee
Pablo <paperez@puce.edu.ec>
2026-05-29 16:24:41 -0500

    Avance Laboratorio 3. Desarrollo de intefaz

--- app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import ec.edu.puce.githubclient.ui.screens.RepoForm
 import ec.edu.puce.githubclient.ui.screens.RepoList
 import ec.edu.puce.githubclient.ui.theme.GithubClientTheme
 
@@ -20,9 +18,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GithubClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RepoList(
-                        modifier = Modifier.padding(innerPadding)
+                var currentScreen by remember { mutableStateOf("repoList") }
+                when (currentScreen) {
+                    "repoList" -> RepoList(
+                        onNavigateToForm = { currentScreen = "repoForm" }
+                    )
+                    "repoForm" -> RepoForm(
+                        onBackClick = { currentScreen = "repoList" }
                     )
                 }
             }
